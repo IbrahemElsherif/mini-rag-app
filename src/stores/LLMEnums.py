c8b662f89ac6666594575348a5c209eab59d0acd
Ibrahem Elsherif <ebrahem.hesham.ahmed@gmail.com>
2025-05-05 16:22:42 +0300

    Update LLMEnums

--- src/stores/LLMEnums.py
+++ src/stores/LLMEnums.py
@@ -5,4 +5,10 @@ class LLMEnums(Enum):
     # names of the providers
     OPENAI= "OPENAI"
     COHERE= "COHERE"
-    GOOGLE= "GOOGLE"
+    GOOGLE= "GOOGLE" 
+    
+class OpenAIEnums(Enum):
+    
+    SYSTEM = "system"
+    USER = "user"
+    ASSISTANT = "assistant"
